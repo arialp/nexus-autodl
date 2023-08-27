5611a63c83384fafb4102d2ab9e7ef44b9c730ae
Alp Eren Arı <arialperen42@outlook.com>
2023-08-28 00:16:31 +0300

    initial commit

--- Makefile
+++ Makefile
@@ -1,26 +1,29 @@
-NAME:=nexus_autodl
+NAME := nexus_autodl
 
 ifeq ($(OS),Windows_NT)
-	PATHSEP:=;
+    PATHSEP := ;
 else
-	PATHSEP:=:
+    PATHSEP := :
 endif
 
-all: yapf lint mypy build
+# Update these paths with the correct paths to your Python executables
+PYTHON_EXEC := PATH-to-python.exe		"%localappdata%\Local\Programs\Python\Python311\python.exe"
+YAPF_PATH := PATH-to-yapf.exe			"%localappdata%\Local\Programs\Python\Python311\yapf.exe"
+MYPY_PATH := PATH-to-mypy.exe			"%localappdata%\Local\Programs\Python\Python311\mypy.exe"
+BUILD_PATH := PATH-to-pyinstaller.exe	"%localappdata%\Local\Programs\Python\Python311\pyinstaller.exe"
+
+all: yapf mypy build
 
 build: $(NAME).py
-	pyinstaller --clean -F --add-data 'templates$(PATHSEP)templates' $<
+	$(BUILD_PATH) --clean -F --add-data templates$(PATHSEP)templates --icon icon.ico $<
 
 clean:
 	$(RM) -r build dist *.spec
 
-lint: $(NAME).py
-	pylint --max-line-length 120 $<
-
 mypy: $(NAME).py
-	mypy $<
+	$(MYPY_PATH) $<
 
 yapf: $(NAME).py
-	yapf -i --style style.yapf $<
+	$(PYTHON_EXEC) $(YAPF_PATH) -i --style style.yapf $<
 
-.PHONY: build clean lint mypy yapf
+.PHONY: build clean mypy yapf
--- nexus_autodl.py
+++ nexus_autodl.py
@@ -10,26 +10,34 @@ import re
 import sys
 import time
 
-from numpy import ndarray as NDArray
+from numpy import ndarray
 import click
-import cv2 as cv  # type: ignore
+import cv2 as cv
 import numpy as np
-import PIL  # type: ignore
-import PIL.ImageOps  # type: ignore
-import pyautogui  # type: ignore
+import pyautogui
+from PIL import Image, ImageOps
 
 
+# These define the main function that runs the autoclicker
 @click.command()
 @click.option('--sleep_max', default=5.)
 @click.option('--sleep_min', default=0.)
+# Main function that runs the autoclicker.
+# Args:
+#    sleep_max (float): Maximum sleep time between auto clicks.
+#    sleep_min (float): Minimum sleep time between auto clicks.
 def run(sleep_max: float, sleep_min: float) -> None:  # pylint: disable=missing-function-docstring
+
+    # Configure logging settings
     logging.basicConfig(
         datefmt='%m/%d/%Y %I:%M:%S %p',
         format='%(asctime)s [%(levelname)s] %(message)s',
         level=logging.INFO,
     )
-    templates = _get_templates()
+    templates = _get_templates()  # Get the list of template images
+    # Run the autoclicker in a loop
     while True:
+        # Generate a random sleep time
         sleep_seconds = random.uniform(sleep_min, sleep_max)
         logging.info('Sleeping for %f seconds', sleep_seconds)
         time.sleep(sleep_seconds)
@@ -39,17 +47,22 @@ def run(sleep_max: float, sleep_min: float) -> None:  # pylint: disable=missing-
             logging.info('Ignoring OpenCV error')
 
 
+# used to store information about a specific template image
 class _Template(NamedTuple):
-    array: NDArray
+    array: ndarray
     name: str
     threshold: int
 
 
+# Define the main function to find and click on templates
+# Find and click on matching templates in the screenshot.
 def _find_and_click(templates: List[_Template]) -> None:
     screenshot_image = pyautogui.screenshot()
     screenshot = _image_to_grayscale_array(screenshot_image)
     for template in templates:
-        sift = cv.SIFT_create()  # pylint: disable=no-member
+        # SIFT patent expired in 2015, so it has been moved to the main OpenCV repository.
+        # "pip install opencv-python" only.
+        sift = cv.SIFT.create()  # pylint: disable=no-member
         _, template_descriptors = sift.detectAndCompute(template.array, mask=None)
         screenshot_keypoints, screenshot_descriptors = sift.detectAndCompute(screenshot, mask=None)
         matcher = cv.BFMatcher()  # pylint: disable=no-member
@@ -67,8 +80,10 @@ def _find_and_click(templates: List[_Template]) -> None:
     logging.info('No matches found')
 
 
+# This responsible for retrieving and preparing template images that will be used for image matching within the Nexus AutoDL script.
+# basically retrieve a list of template images from the templates directory.
 def _get_templates() -> List[_Template]:  # pylint: disable=too-many-locals
-    templates = []
+    templates: List[_Template] = []  # Add the type annotation here
     try:
         root_dir = sys._MEIPASS  # type: ignore  # pylint: disable=no-member,protected-access
     except AttributeError:
@@ -82,18 +97,20 @@ def _get_templates() -> List[_Template]:  # pylint: disable=too-many-locals
     sorted_groups = sorted(groups, key=lambda t: int(t[0]))
     for index, threshold, name in sorted_groups:
         path = os.path.join(templates_dir, f'{index}_{threshold}_{name}.png')
-        image = PIL.Image.open(path)  # pylint: disable=no-member
+        image = Image.open(path)
         array = _image_to_grayscale_array(image)
         template = _Template(array=array, name=name, threshold=int(threshold))
         templates.append(template)
     return templates
 
 
-def _image_to_grayscale_array(image: PIL.Image.Image) -> NDArray:
-    image = PIL.ImageOps.grayscale(image)
+# Convert a PIL Image to a grayscale NumPy array.
+def _image_to_grayscale_array(image: Image.Image) -> ndarray:
+    image = ImageOps.grayscale(image)
     array = np.array(image)
     return array
 
 
+# Execute the main function only if the script is run directly
 if __name__ == '__main__':
     run()  # pylint: disable=no-value-for-parameter
